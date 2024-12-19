f229028425e5cd0c6c321fff1090d7866fe69429
TheodoreBillotte <theodore.billotte@epitech.eu>
2024-12-19 22:11:19 +0100

    add: player health

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -6,10 +6,13 @@ const ACCELERATION = 400
 const JUMP_VELOCITY = -400.0
 const ROTATION_SPEED = 2
 @onready var camera = $Camera2D
+@onready var health_component : HealthComponent = $HealthComponent
 
 func _physics_process(delta: float) -> void:
 	move_and_collide(velocity * delta)
 
+func _on_health_component_on_death() -> void:
+	queue_free()
 
-func _on_timer_timeout() -> void:
-	pass # Replace with function body.
+func _on_health_component_on_damage(damage: int) -> void:
+	print("Took", damage, "damage")
