f93c3ffc8db9998e4fc009c20fde7f0d7f0106b3
guillaume1.tran@epitech.eu <guillaume1.tran@epitech.eu>
2024-12-19 18:51:10 +0100

    add better enemy

--- entities/enemies/enemies.gd
+++ entities/enemies/enemies.gd
@@ -1,8 +1,15 @@
 extends CharacterBody2D
 class_name Enemy
 
+@onready var health_component : HealthComponent = $HealthComponent
+
 func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func _kill() -> void:
 	queue_free()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+		
+	pass # Replace with function body.
