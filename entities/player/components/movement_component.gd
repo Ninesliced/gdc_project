9bfeef23a9a44035364a3fbbf43a37611279fe94
guillaume1.tran@epitech.eu <guillaume1.tran@epitech.eu>
2024-12-19 17:07:52 +0100

    added player with movement

---
+++ entities/player/components/movement_component.gd
@@ -0,0 +1,51 @@
+extends Node2D
+
+@export var speed = 300.0
+@export var acceleration = 200
+@export var decceleration = 100
+@export var rotation_speed = 2
+@onready var parent = null
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	parent = get_parent()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var direction = Vector2.ZERO
+	var velocity = parent.velocity
+
+	if Input.is_action_pressed("down"):
+		direction.y = 1
+	if Input.is_action_pressed("up"):
+		direction.y = -1
+	if Input.is_action_pressed("left"):
+		parent.rotate(-delta * rotation_speed)
+	if Input.is_action_pressed("right"):	
+		parent.rotate(delta * rotation_speed)
+	
+	direction = direction.rotated(parent.rotation)
+	velocity = handle_acceleration(direction,velocity, delta)
+	velocity = handle_decceleration(direction, velocity, delta)
+	parent.velocity = velocity
+	pass
+
+func handle_acceleration(direction,velocity, delta):
+	if direction.x > 0:
+		velocity.x = velocity.move_toward(direction * speed, acceleration * delta).x
+	if direction.x < 0:
+		velocity.x = velocity.move_toward(direction * speed, acceleration * delta).x
+	if direction.y > 0:
+		velocity.y = velocity.move_toward(direction * speed, acceleration * delta).y
+	if direction.y < 0:
+		velocity.y = velocity.move_toward(direction * speed, acceleration * delta).y
+	
+	return velocity
+
+func handle_decceleration(direction, velocity, delta):
+	if direction.x == 0:
+		velocity.x = velocity.move_toward(Vector2.ZERO,decceleration * delta).x
+	if direction.y == 0:
+		velocity.y = velocity.move_toward(Vector2.ZERO,decceleration * delta).y
+	return velocity
---
+++ entities/player/player.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+class_name Player
+
+const SPEED = 20000.0
+const ACCELERATION = 400
+const JUMP_VELOCITY = -400.0
+const ROTATION_SPEED = 2
+@onready var camera = $Camera2D
+
+func _physics_process(delta: float) -> void:
+	move_and_slide()
+
+
+func _on_timer_timeout() -> void:
+	pass # Replace with function body.
