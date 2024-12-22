06b64b51fa79169cc03c03ffb8f51d637664bc7e
Guigui <113891088+Notgoyome@users.noreply.github.com>
2024-12-22 16:26:12 +0100

    Update health component to show HP progress bar

--- entities/components/health_component.gd
+++ entities/components/health_component.gd
@@ -1,10 +1,19 @@
+@tool
 extends Node2D
 class_name HealthComponent
 
 @export var max_hp := 100
 @export var initial_hp := 100
 @export var invincible := false
+@export var show_hp = false :
+	set(value):
+		$ProgressBar.visible = value
+		$ProgressBar.value = hp
+		show_hp = value
+
 var hp = 0
+
+@onready var progress_bar : TextureProgressBar = $ProgressBar
 
 signal on_damage(damage: int)
 signal on_death
@@ -12,6 +21,15 @@ signal on_heal(heal: int)
 
 func _ready():
 	hp = initial_hp
+	progress_bar.visible = show_hp
+	progress_bar.max_value = max_hp
+	progress_bar.value = hp
+	if progress_bar.visible:
+		set_process(true)
+
+func _process(delta):
+	if progress_bar.visible:
+		global_rotation = 0
 
 func damage(n):
 	if invincible:
@@ -21,6 +39,8 @@ func damage(n):
 	if hp <= 0:
 		hp = 0
 		emit_signal("on_death")
+	progress_bar.value = hp
+	print(hp)
 	pass 
 
 func heal(n):
