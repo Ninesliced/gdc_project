e1a9fcd54efcfaeec75a908560137bac242402aa
Guigui <113891088+Notgoyome@users.noreply.github.com>
2024-12-22 17:01:10 +0100

    Update basic_drop.tscn, basic_enemy.tscn, and main.tscn files

---
+++ entities/enemies/drops/components/magnet_drop_component.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+@export var follower : Node2D 
+var follow : Node2D = null
+var speed : float = 200
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if follower == null:
+		follower = get_parent()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if follow != null:
+		var direction = follow.global_position - global_position
+		direction = direction.normalized()
+		follower.global_position += direction * speed * delta
+	pass
+
+
+func _on_body_entered(body:Node2D) -> void:
+	if body is Player:
+		follow = body
+	pass # Replace with function body.
