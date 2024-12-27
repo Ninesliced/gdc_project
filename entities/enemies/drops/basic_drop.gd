74df2c6075c4d9dbf6a79cc40408624770fc3b2d
Guigui <113891088+Notgoyome@users.noreply.github.com>
2024-12-27 15:55:23 +0100

    Update basic_drop.gd and basic_drop.tscn: Add audio and sprite functionality

--- entities/enemies/drops/basic_drop.gd
+++ entities/enemies/drops/basic_drop.gd
@@ -1,7 +1,12 @@
 extends Node2D
 
 @export var data: CounterData
+@onready var audio_pick = $AudioStreamPlayer2D
+@onready var sprite = $Galaxy
 
 func _on_collectible_collected(player: Player) -> void:
 	player._player_counter._add(data)
+	audio_pick.play()
+	sprite.queue_free()
+	await audio_pick.finished
 	queue_free()
