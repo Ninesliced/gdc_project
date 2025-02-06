b7119c6c326ba67613851b471d24984c4fbd5e1e
Guillame Tran <guillaume1.tran@epitech.eu>
2025-02-06 14:09:41 +0100

    FIX game

--- globals/game_data.gd
+++ globals/game_data.gd
@@ -1,9 +1,9 @@
 extends Node
 
 var list_available_characters : Array[PlayerStats] = [
-	preload("res://entities/player/player_stats/player_base.tres"),
+	load("res://entities/player/player_stats/player_base.tres"),
 	# preload("res://entities/player/player_stats/player_debug.tres"),
-	preload("res://entities/player/player_stats/the_moon.tres")
+	load("res://entities/player/player_stats/the_moon.tres")
 ]
 
 var name_colors : Dictionary = {
